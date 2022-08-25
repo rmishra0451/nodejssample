d77ee5706d153985d30f52d64475f5c3485272b1
rmishra0451 <rmishra@truperformance.us>
2022-08-25 19:12:06 +0530

    minor changes.

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -6,11 +6,6 @@ sudo chmod -R 777 /home/ubuntu/express-app
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/express-app
 
-#add npm and node to pathexport NVM_DIR="$HOME/.nvm"
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
-
 #install node modules
 npm install
 
