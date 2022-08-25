c3d64ff1f9daf2ce8c96fffef65e890424cccc87
rmishra0451 <rmishra@truperformance.us>
2022-08-25 18:33:30 +0530

    modified script.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,4 +2,10 @@
 
 #installing node version manager 
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+
+#add npm and node to pathexport NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
+
 nvm install node
