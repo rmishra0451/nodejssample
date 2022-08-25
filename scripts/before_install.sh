7838293f4ad3a27f3409f25cbdc731fab73fb7ff
rmishra0451 <rmishra@truperformance.us>
2022-08-25 17:11:28 +0530

    changed nvm install source.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #installing node version manager 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 nvm install node
 
 #create the working directory if it does not exist
