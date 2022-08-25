76a869c7e1610ed8bab2215b455f816874d97e37
rmishra0451 <rmishra@truperformance.us>
2022-08-25 18:26:32 +0530

    removed after command comments.

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -6,10 +6,10 @@ sudo chmod -R 777 /home/ubuntu/express-app
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/express-app
 
-#add npm and node to path
+#add npm and node to pathexport NVM_DIR="$HOME/.nvm"
 export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
 
 #install node modules
 npm install
