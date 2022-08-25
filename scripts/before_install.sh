51ef6481951978ab7b83f9e09eb2ff3858cf57e9
rmishra0451 <rmishra@truperformance.us>
2022-08-25 15:13:16 +0530

    pushing app to git.

---
+++ scripts/before_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#installing node version manager 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+
+#create the working directory if it does not exist
+DIR="/home/ec2-user/express-app"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
