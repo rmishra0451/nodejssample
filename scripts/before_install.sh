62743fde5f88d779d01b7c650e3510021789eb07
rmishra0451 <rmishra@truperformance.us>
2022-08-25 18:18:51 +0530

    added repo name in sources.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,12 +3,3 @@
 #installing node version manager 
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 nvm install node
-
-#create the working directory if it does not exist
-DIR="/home/ubuntu/express-app"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
