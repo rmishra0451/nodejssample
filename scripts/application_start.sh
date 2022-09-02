ea02cad42dd003576d55fdbf8f7ed07b8cc0d7e0
rmishra0451 <rmishra@truperformance.us>
2022-09-02 16:52:05 +0530

    minor changes.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 700 /home/ubuntu/express-app
+sudo chmod -R 070 /home/ubuntu/express-app
 
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/express-app
